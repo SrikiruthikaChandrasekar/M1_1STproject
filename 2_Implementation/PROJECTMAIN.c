c997cf54dfb3c5e445da40400cd6a197eb529371
Sri kiruthika Chandrasekar <101560798+SrikiruthikaChandrasekar@users.noreply.github.com>
2022-03-22 09:22:37 +0530

    Update PROJECTMAIN.c

--- 2_Implementation/PROJECTMAIN.c
+++ 2_Implementation/PROJECTMAIN.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include<math.h>
 int main() 
 {
   char category;
+  int n, *ptr;
   float VolumeChoice;
   float TimeChoice;
   float EnergyChoice;
@@ -33,17 +35,34 @@ int main()
       if(VolumeChoice == 1){
           printf("Please enter the Militre value you want to convert: \n");
           scanf("%f",&userinputml);
+          ptr = (int*) malloc(n * sizeof(int));
+          if(ptr == NULL)
+          {
+              printf("Error! memory not allocated.");
+              exit(0);
+          }
           MililitretoLitre=  ((userinputml / 1000));
           printf("Litre: %f",MililitretoLitre);
+         
+         
+          free(ptr);
+          
       }
       else if(VolumeChoice == 2){
         printf("Please enter the Litre value you want to convert: \n");
         scanf("%f",&userinputL);
+        ptr = (int*) malloc(n * sizeof(int));
+          if(ptr == NULL)
+          {
+              printf("Error! memory not allocated.");
+              exit(0);
+          }
         LitretoMililitre = (userinputL*1000);
         printf("Mililitre: %f",LitretoMililitre);
       }
       else
         printf("Please enter the correct Choice. \n");
+        free(ptr);
   }
   
   else if(category == 'T') {
@@ -55,17 +74,31 @@ int main()
       if(TimeChoice == 1){
           printf("Please enter the Minutes value to convert: \n");
           scanf("%f",&userinputMinutes);
+          ptr = (int*) malloc(n * sizeof(int));
+          if(ptr == NULL)
+          {
+              printf("Error! memory not allocated.");
+              exit(0);
+          }
           Minutestoseconds = userinputMinutes * 60;
           printf("Converted the given Minutes into seconds: %f",Minutestoseconds);
+          free(ptr);
       }
       else if(TimeChoice == 2){
           printf("Please enter the Seconds value to convert: \n");
           scanf("%f",&userinputSeconds);
+          ptr = (int*) malloc(n * sizeof(int));
+          if(ptr == NULL)
+          {
+              printf("Error! memory not allocated.");
+              exit(0);
+          }
           SecondstoMinutes = (userinputSeconds * (1/60));
           printf("Converted the given Seconds to minutes: %f",SecondstoMinutes);
       }
       else
         printf("Please enter the correct choice. \n");
+        free(ptr);
    }
   else if(category == 'E'){
       printf("Welcome to Energy unit conversion! \n");
@@ -73,19 +106,39 @@ int main()
       printf("Enter 1 for Calories to Joules \n");
       printf("Enter 2 for Joules to Calories \n");
       scanf("%f",&EnergyChoice);
+      ptr = (int*) malloc(n * sizeof(int));
+          if(ptr == NULL)
+          {
+              printf("Error! memory not allocated.");
+              exit(0);
+          }
       if(EnergyChoice == 1){
           printf("Please enter the Calories value: \n");
           scanf("%f",&userinputCal);
+          ptr = (int*) malloc(n * sizeof(int));
+          if(ptr == NULL)
+          {
+              printf("Error! memory not allocated.");
+              exit(0);
+          }
            CaltoJoules= userinputCal * 4184;
           printf("Joules: %f",CaltoJoules);
+          free(ptr);
       }
       else if(EnergyChoice == 2) {
           printf("Please enter the Joules value: \n");
           scanf("%f",&userinputJoule);
+          ptr = (int*) malloc(n * sizeof(int));
+          if(ptr == NULL)
+          {
+              printf("Error! memory not allocated.");
+              exit(0);
+          }
           JoulestoCal = (userinputJoule/4184) ;
           printf("Calories: %f",JoulestoCal);
+          free(ptr);
       }
       else 
         printf("Please enter the correct choice. \n");
+        }
    }
-  return 0;
