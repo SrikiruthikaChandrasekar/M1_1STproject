9b4c89c0893d13924282c557145b75dc844ddf96
Sri kiruthika Chandrasekar <101560798+SrikiruthikaChandrasekar@users.noreply.github.com>
2022-03-23 15:43:26 +0530

    Update PROJECTMAIN.c

--- 2_Implementation/PROJECTMAIN.c
+++ 2_Implementation/PROJECTMAIN.c
@@ -93,7 +93,7 @@ int main()
               printf("Error! memory not allocated.");
               exit(0);
           }
-          SecondstoMinutes = (userinputSeconds * (1/60));
+          SecondstoMinutes = (userinputSeconds / 60));
           printf("Converted the given Seconds to minutes: %f",SecondstoMinutes);
       }
       else
